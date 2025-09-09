ac16a7760d46feb6f87b78257cdb3211ea488a55
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-09 16:35:44 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,18 @@
+package DAY2;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        String nama = "Nayla";
+        int umur = 18;
+        double BB = 45.0;
+        
+        System.out.println("Nama:"+nama);
+        System.out.print("umur:"+umur );
+        System.out.print("\n");
+        System.out.printf("Nama saya %s, BB saya %.1f, dan umur saya %dtahun.",nama,BB,umur);
+        
+
+    }
+    
+}
