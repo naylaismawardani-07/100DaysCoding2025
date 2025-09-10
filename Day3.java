80445b086d599c0376a14ea707fc7d7bdac39586
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-10 15:10:25 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,25 @@
+package Day3;
+
+public class Main {
+    
+    public static void main(String[] args) {
+       /*Kommetar hari ini yaitu komentar
+         Jenis komentar itu ada 3 jenis yaitu
+         yg pertama itu ada:
+        */
+        
+        // 1. Komentar ini untuk satu baris
+        
+        /* 2. Komentar ini untuk 
+           beberapa baris
+        */
+           System.out.println("BY NAYLA ISMAWARDANI");
+         
+        /*komentar ini tidak akan dibaca oleh program
+           ketika dijalankan.ini hanya catatan untuk
+           kita agar mudah mengingat kode apa yg kita 
+           masukkan
+         */
+    }
+    
+}
