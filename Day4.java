c0ba90082af7e9637010dea615ee770383ee854b
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-11 16:49:45 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,24 @@
+package Day4;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Escape Sequence : \t,\n,\b,\\,\",\'
+         System.out.println("Nama\t: Nayla Ismawardani");
+         System.out.println("Umur\t: 18\n\t  tahun");
+         System.out.println("Saya suka warna pink\\merah muda");
+         System.out.println("Saya suka \"menjual\"");
+         System.out.println("Jangann\b lupa sholat");
+         System.out.println("saya bisa dipanggil ila\'");
+        
+        //Fungsi dari masing2 escape sequence tersebut adalah:
+        /* \t (tab atau jarak spasi)
+         * \n (enter,newline atau pindah baris)
+         * \b (backspace, menghapus satu karakter)
+         * \\ (menampilkan backslash)
+         * \" (menampilkan tanda kutip ganda)
+         * \' (menampilkan tanda kutip tunggal)
+         */
+       }
+    
+}
