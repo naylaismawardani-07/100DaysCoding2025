a8481a0e539a62b87708d4e22ed6fc51d121f0dc
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-12 15:05:00 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,26 @@
+package Day5;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /* Commit hari ini
+           Tipe data numerik (bilangan bulat)
+           yaitu byte,int,short,dan long
+        */
+        //tipe data bilangan bulat
+        byte saudara = 2;
+        int anak = 2;
+        short tahun = 2006;
+        long tempuh = 10000;
+        
+        //untuk menampilkan (output)
+        System.out.println("saya mempunyai saudara " + saudara);
+        System.out.println("saya anak ke-" + anak);
+        System.out.println("saya lahir pada tahun " + tahun);
+        System.out.println("jarak tempuh " + tempuh+" km");
+        
+        //perbedaan tipe data tersebut adalah
+        //kapasitas tiap2 tipe data yg bisa ditampung (ukuran)
+    }
+    
+}
