1fff49f6ea8ce9325ecf2c06bce9745efcea7ce9
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-13 11:38:47 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,24 @@
+package Day6;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         //commit hari ini 
+         //tipe data pecahan (double dan float)
+         
+        //tipe data pecahan
+        float beratbadan = 45.5f;
+        double tinggibadan = 153.5;
+        
+        //untuk menampilkan (output)
+        System.out.println("berat badan saya " + beratbadan+" kg");
+        System.out.println("tinggi badan saya " + tinggibadan+" cm");
+        
+        /*tipe data tersebut mempunyai perbedaan 
+         * yaitu sama dengan tipe data bilangan bulat
+         * berbeda di kapasitas float lebih kecil dibanding double
+         */
+    }
+    
+    
+}
