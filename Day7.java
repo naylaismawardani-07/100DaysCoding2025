243346edb2b2d26f061df4cb2881bdb86c9a2c0f
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-14 10:52:07 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,23 @@
+package Day7;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //tipe data karakter (char) dan boolean
+        char gender = 'p';
+        boolean status = true;
+        
+        //untuk menampilkan
+        System.out.println("gender \t\t:"+gender);
+        System.out.println("mahasiswa\t:"+status);
+        
+        /* tipe data char itu digunakan untuk menyimpan satu
+         * karakter tunggal ditandai dengan tanda kutip tunggal ''
+         * dan tipe data boolean digunakan untuk
+         * menyatakan suatu kondisi benar (true) atau salah (false).
+         */
+        
+        
+    }
+    
+}
