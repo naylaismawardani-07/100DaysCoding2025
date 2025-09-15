521fbcd34c82d526fc9285b95c439d3c065947a6
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-15 17:21:28 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,21 @@
+package Day8;
+
+public class Main {
+    
+    public static void main(String[] args) {
+            //commit hari ini tipe data string
+        
+        String nama = "Nayla Ismawardani";
+        String kelas = "C Informatika";
+        System.out.println("nama \t:" +nama);
+        System.out.println("kelas \t:" +kelas);
+        
+        /*tipe data string adalah tipe data non primitif
+         * tipe data string digunakan untuk menyimpan kata atau kalimat
+         * ditandai dengan tanda kutip ganda 
+         */
+        
+        
+            }
+    
+}
