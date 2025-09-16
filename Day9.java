d7a6c84680b7761cddf2e5cf017b60f373152c11
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-16 20:55:20 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+package Day9;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        byte jumlah; //Deklarasi
+        jumlah = 2; //Inisialisasi
+        jumlah = 3; // Update variabel
+        System.out.println(jumlah);
+        
+        /* Deklarasi itu adalah membuat tipe data dan variabelnya.
+         * Inisialisasi itu adalah memberikan nilai pada variabel 
+         * yang sudah dideklarasikan.
+         * Update variabel itu adalah mengubah nilai variabel yg sudah
+         * dibuat sebelumnya.
+         */
+    }
+    
+}
