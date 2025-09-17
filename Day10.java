a52a05cf4b222df6ea47902b786e47748c050fa2
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-17 09:13:19 +0800

    Day10.java

---
+++ Day10.java
@@ -0,0 +1,15 @@
+package Day10;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        final int harga = 10000;
+        /*constanta ( final variabel) adalah variabel yang nilainya tidak bisa diubah
+         * lagi setelah dideklarasikan dengan menambahkan kata kunci 
+         * final sebelum tipe data dan variabel.
+         */
+        System.out.println("Harganya Rp" + harga);
+        
+    }
+    
+}
