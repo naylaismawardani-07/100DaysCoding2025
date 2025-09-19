b48b4ba5f5baae4e42e215fd102f62c9c2e24576
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-19 11:53:43 +0800

    Day12.java

---
+++ Day12.java
@@ -0,0 +1,24 @@
+package Day12;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        //Biodata
+        String Nama = "Nayla Ismawardani";
+        String NIM = "D0225022";
+        int Umur = 18;
+        char Kelas = 'C';
+        String Alamat = "Papalang";
+        Double TinggiBadan = 153.5;
+        Double BeratBadan = 45.3;
+
+        System.out.println("Nama \t\t: " + Nama);
+        System.out.println("NIM \t\t: " + NIM);
+        System.out.println("Umur \t\t: " + Umur);
+        System.out.println("Kelas \t\t: " + Kelas + " Informatika");
+        System.out.println("Alamat \t\t: " + Alamat);
+        System.out.println("Tinggi Badan \t: " + TinggiBadan);
+        System.out.println("Berat Badan \t: " + BeratBadan);
+    }
+    
+}
