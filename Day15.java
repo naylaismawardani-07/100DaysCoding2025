6ac5e30e64841d835b9e9ad5b5af79b0ba0ae328
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-22 21:06:50 +0800

    Day15.java

---
+++ Day15.java
@@ -0,0 +1,17 @@
+package Day15;
+import java.util.Scanner;
+public class f {
+    public static void main(String[] args) {   
+    Scanner input = new Scanner(System.in); 
+        System.out.print("Masukkan Angka 1 : ");
+        int a = input.nextInt();
+        System.out.print("Masukkan Angka 2 : ");
+        int b = input.nextInt();
+
+        int Penjumlahan = a + b;
+        System.out.println("Hasil Penjumlahan " + Penjumlahan);
+        
+        int Pengurangan = a - b;
+        System.out.println("Hasil Pengurangan " + Pengurangan);
+}
+}
