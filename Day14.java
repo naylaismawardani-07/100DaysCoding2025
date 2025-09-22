0ac1d69c90e8ce3b1506c3178ea3f758a4326a10
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-22 20:14:04 +0800

    Day14.java

--- Day14.java
+++ Day14.java
@@ -6,40 +6,42 @@ public class Main {
         //Mengubah String menjadi Tipe Data Primitif
         String Nomor = "12";
         int angka = Integer.parseInt(Nomor);
-        System.out.println(Nomor);
+        System.out.println("Nomor : " + angka);
         
         String Huruf = "1";
         byte nama = Byte.parseByte(Huruf);
-        System.out.println(Huruf);
+        System.out.println("Huruf : " + nama);
         
         String Jam = "15";
         short bel = Short.parseShort(Jam);
-        System.out.println(Jam);
+        System.out.println("Jam : " +bel);
         
         String Del = "2";
         long dek = Long.parseLong(Del);
-        System.out.println(Del);
+        System.out.println("Del : " +dek);
         
         String Adek = "0.5";
         double kk = Double.parseDouble(Adek);
-        System.out.println(Adek);
+        System.out.println("Adek : "+kk);
         
         String Kakak = "2.1f";
         float dede = Float.parseFloat(Kakak);
-        System.out.println(Kakak);
+        System.out.println("Kakak : "+dede);
         
         String mama = "p";
         char papa = mama.charAt(0);
-        System.out.println(mama);
+        System.out.println("Mama : " +papa);
         
         String Nenek = "true";
         boolean kakek = Boolean.parseBoolean(Nenek);
-        System.out.println(Nenek);
+        System.out.println("Nenek : "+ kakek);
         
         /* Mengubah String menjadi tipe data primitif adalah
          * proses mengubah teks menjadi angka, huruf, atau logika 
          * (int, double, char, boolean) agar bisa diproses program, 
-         * dengan menggunakan method parse atau charAt dan ini digunakan sesuai kebutuhan
+         * dengan menggunakan method parse atau charAt
          */
     }
+    
+    
 }
