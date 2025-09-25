8ea166eaa9bb9f55ce41b47b25d07c29fa1631cd
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-25 21:30:09 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,13 @@
+package Day18;
+
+public class Main {
+    
+    public static void main(String[] args) {
+         int Nay = 10;
+         long Nayla = Nay;
+         byte Ila = 2;
+         short Nela = Ila;
+        System.out.println("Nilai int   \t: " + Nay);
+        System.out.println("Nilai byte  \t: " + Nela);
+    }
+}
