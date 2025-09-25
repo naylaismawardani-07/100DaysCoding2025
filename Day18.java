354c10bb77a8fbb9aebe98ffd010a8c1b925583a
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-25 21:58:54 +0800

    Day18.java

--- Day18.java
+++ Day18.java
@@ -3,11 +3,13 @@ package Day18;
 public class Main {
     
     public static void main(String[] args) {
+        //Konversi otomatis (tipe data kecil ke besar)
+        //Wide casting
          int Nay = 10;
          long Nayla = Nay;
          byte Ila = 2;
          short Nela = Ila;
-        System.out.println("Nilai int   \t: " + Nay);
-        System.out.println("Nilai byte  \t: " + Nela);
+        System.out.println("Int ke long  \t: " + Nay);
+        System.out.println("byte ke short \t: " + Nela);
     }
 }
