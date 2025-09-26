f638badd2f36b4214cc6f21fc9de79fb7ea76738
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-26 21:01:30 +0800

    Day19.java

--- Day19.java
+++ Day19.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         long Nina = 10000000l;
         int Nayla = (int)Nina;
-        System.out.println("Nilai long"+Nina);
-        System.out.println("Nilai Int"+Nayla);
+        System.out.println("Nilai long ke int"+Nayla);
+        
     }
 }
