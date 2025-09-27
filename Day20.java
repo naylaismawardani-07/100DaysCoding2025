98a08780de047ddab4f2ac523c15e6b2b04c14f3
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-09-27 19:43:02 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,18 @@
+package Day20;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int Kamu = 90;
+        String Kamuu = String.valueOf(Kamu);
+        System.out.println("int ke string : "+Kamuu);
+        
+        int Saya = 100;
+        String Sayaa = Integer.toString(Saya);
+        System.out.println("int ke string 2 : "+Sayaa);
+        
+        byte Dia = 1;
+        String Diaa = ""+Dia;
+        System.out.println("Byte ke String : "+Diaa);
+    }
+}
