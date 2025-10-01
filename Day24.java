1e4f4e069f721e57792c1205fdc21b899d66e47e
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-01 14:17:07 +0700

    Day24.java

---
+++ Day24.java
@@ -0,0 +1,14 @@
+package Belajar;
+
+import java.util.Scanner;
+
+public class Day24 {
+    public static void main(String[] args) {
+        Scanner nay = new Scanner(System.in);
+        System.out.print("masukkan jari-jari lingkaran : ");
+        double r = nay.nextDouble();
+        
+        double Luas = Math.PI * r * r;
+        System.out.println("Luas lingkaran dengan jari-jari " + r + " adalah : " + Luas);
+    }
+}
