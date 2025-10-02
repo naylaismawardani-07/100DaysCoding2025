f3182db0b730b10e34b0ad2e783841a3ea2a53e1
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-02 20:40:40 +0700

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,15 @@
+package Belajar;
+import java.util.Scanner;
+public class Day25 {
+    public static void main(String[] args) {
+        Scanner nay = new Scanner(System.in);
+        System.out.print("masukkan angka : ");
+        int i = nay.nextInt();
+
+        i++;
+        System.out.println("increment : " + i);
+
+        i--;
+        System.out.println("decrement : " + i);
+    }
+}
