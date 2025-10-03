2a066000a1e2936e6b70960d5f169342429a9263
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-03 20:01:27 +0700

    Day26.java

---
+++ Day26.java
@@ -0,0 +1,22 @@
+package Belajar;
+
+public class Day26 {
+    public static void main(String[] args){
+    int a;
+    int b;
+
+    a = 15;
+    b = 10;
+
+    a += b;
+    System.out.println("Penjumlahan : " + a);
+    a -= b;
+    System.out.println("Pengurangan : " + a);
+    a *= b;
+    System.out.println("Perkalian : " + a);
+    a /= b;
+    System.out.println("Pembagian : " + a);
+    a %= b;
+    System.out.println("Modulus/sisa bagi : " + a);
+    }  
+}
