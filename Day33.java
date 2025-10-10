a5436d0fd8accf01daf7ce1337a02b52b95950e9
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-10 19:56:17 +0700

    Day33.java

--- Day33.java
+++ Day33.java
@@ -5,10 +5,12 @@ import java.util.Scanner;
 public class Day33 {
     public static void main(String[] args) {
         Scanner n= new Scanner(System.in);
-        System.out.print("Masukkan nilai  : ");
+        System.out.print("Masukkan nilai 1 : ");
         int nilai1 = n.nextInt();
+        System.out.print("Masukkan nilai 2 : ");
+        int nilai2 = n.nextInt();
 
-        boolean lulus = !(nilai1 >= 75);
+        boolean lulus = !((nilai1 >= 75)|| (nilai2 >= 80));
         System.out.println("Apakah kamu lulus? "+lulus);
     }   
 }
