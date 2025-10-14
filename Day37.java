40e163c9f827bf29d8f3db55a9a9b8e7689cc62b
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-14 20:09:33 +0700

    Day37.java

--- Day37.java
+++ Day37.java
@@ -5,13 +5,23 @@ import java.util.Scanner;
 public class Day37 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
-        System.out.print("Masukkan angka : ");
+        System.out.print("Masukkan angka(3 digit) : ");
         int angka = n.nextInt();
-
-        if(angka %2 != 0){
-            System.out.println("Angka "+angka+ " termasuk bilangan ganjil");
+       
+        if((angka % 10) %2 == 0){
+            System.out.println("Angka satuaannya genap");
         }else{
-            System.out.println("Angka "+angka+ " termasuk bilangan genap");
+            System.out.println("Angka satuannya ganjil");
+        }
+        if (((angka / 10) % 10) % 2 == 0){
+            System.out.println("Angka puluhannya genap");
+        }else{
+            System.out.println("Angka puluhannya ganjil");
+        }
+        if(((angka / 100 ) % 10) % 2 == 0){
+            System.out.println("Angka ratusannya genap");
+        }else{
+            System.out.println("Angka ratusannya ganjil");
         }
     }
     
