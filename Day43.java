c617af2fb737844c9023ff2460aea146b1b1fda5
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-20 20:17:24 +0700

    Day43.java

--- Day43.java
+++ Day43.java
@@ -11,14 +11,18 @@ public class Day43 {
         String b = "";
         String c = "";
 
+        if (angka < 0){
+            System.out.println("Angka tidak boleh negatif");
+            return;
+        }
+
         if (angka < 10){
             a = "satuan";
         }else if (angka < 100){
             a = "puluhan";
-        }else if (angka < 0){
-            System.out.println("Angka tidak boleh negatif");
-        }else {
+        }else if (angka < 1000){
             a = "ratusan";
+        }
 
         if (angka %2 == 0){
             b = " genap";
@@ -32,7 +36,7 @@ public class Day43 {
         }else if (angka %5 == 0 ){
             c = " bisa dibagi 5";
         }else{
-            c = "tidak bisa dibagi 3 dan 5";
+            c = " tidak bisa dibagi 3 dan 5";
         }
         System.out.println(angka+" adalah angka "+a+""+b+" yang"+c);
     }   
