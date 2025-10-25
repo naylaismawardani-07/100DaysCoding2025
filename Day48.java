04b315c527ded8fba93d11cbfbd711d08ab9ac61
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-25 18:35:22 +0700

    Day48.java

--- Day48.java
+++ Day48.java
@@ -6,42 +6,39 @@ public class Day48 {
     public static void main(String[] args) {
         Scanner n = new Scanner(System.in);
         System.out.println("=== KALKULATOR SEDERHANA ===");
-        System.out.print("");
         int angka1 = n.nextInt();
-        System.out.print("");
         char operator = n.next().charAt(0);
-        System.out.print("");
         int angka2 = n.nextInt();
+        boolean valid = true;
         int hasil = 0;
 
         switch (operator) {
             case '+':
                 hasil = angka1 + angka2;
-                System.out.println("Hasil dari "+angka1+"+"+angka2+" adalah " + hasil);
                 break;
 
             case '-':
                 hasil = angka1 - angka2;
-                System.out.println("Hasil dari "+angka1+"-"+angka2+" adalah " + hasil);
                 break;
 
             case '*':
                 hasil = angka1 * angka2;
-                System.out.println("Hasil dari "+angka1+"*"+angka2+" adalah " + hasil);
                 break;
 
             case '/':
                 hasil = angka1 / angka2;
-                System.out.println("Hasil dari "+angka1+"/"+angka2+" adalah " + hasil);
                 break;
 
             case '%':
                 hasil = angka1 % angka2;
-                System.out.println("Hasil dari "+angka1+"%"+angka2+" adalah "+hasil);
                 break;
 
             default:
                 System.out.println("Operator tidak valid!");
+                valid = false;
+        }
+        if (valid){
+        System.out.println("Hasil dari "+angka1+" "+operator+" "+angka2+" adalah "+hasil);
         }
     }
 }
