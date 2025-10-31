b9bf267a0c39eaa4ad7a86a8f9b53d47c8850b5b
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-10-31 19:41:28 +0700

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,49 @@
+ package Belajar;
+
+import java.util.Scanner;
+
+public class Day54 {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+
+        int jumlahMahasiswa = 10;
+        double totalValid = 0;
+        int jumlahValid = 0;
+        int jumlahTidakValid = 0;
+        double nilaiTerkecil = 100;
+        double nilaiTerbesar = 0;
+
+        for (int i = 1; i <= jumlahMahasiswa; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
+            double nilai = n.nextDouble();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println(" Nilai tidak valid dan tidak diproses.");
+                jumlahTidakValid++;
+                continue; 
+            }
+            totalValid += nilai;
+            jumlahValid++;
+
+            if (nilai < nilaiTerkecil) {
+                nilaiTerkecil = nilai;
+            }
+
+            if (nilai > nilaiTerbesar) {
+                nilaiTerbesar = nilai;
+            }
+        }
+        System.out.println("Total nilai valid       : " + totalValid);
+        System.out.println("Jumlah nilai valid      : " + jumlahValid);
+        System.out.println("Jumlah nilai tidak valid: " + jumlahTidakValid);
+
+        if (jumlahValid > 0) {
+            double rataRata = totalValid / jumlahValid;
+            System.out.println("Nilai valid terkecil    : " + nilaiTerkecil);
+            System.out.println("Nilai valid terbesar    : " + nilaiTerbesar);
+            System.out.println("Rata-rata nilai valid   : " + rataRata);
+        } else {
+            System.out.println("Tidak ada nilai valid yang diproses.");
+        }
+    }
+}
