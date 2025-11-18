107e9397de23602c88309d7a5a733e037107e566
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-11-18 16:42:45 +0700

    Day72.java

---
+++ Day72.java
@@ -0,0 +1,19 @@
+package Belajar;
+
+import java.util.Scanner;
+
+public class Day72 {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.print("Masukkan tinggi segitiga : ");
+        int tinggi = n.nextInt();
+
+        for (int i = tinggi; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
