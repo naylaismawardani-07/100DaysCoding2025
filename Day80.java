c90f5d206eee50792929dbeafcd84b64b28e4edd
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-11-26 21:17:18 +0800

    Day80.java

---
+++ Day80.java
@@ -0,0 +1,13 @@
+package Day80;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        
+        System.out.println("Isi array:");
+        for(int i = 0; i < angka.length; i++){
+            System.out.println("Indeks " + i + " = " + angka[i]);
+        }
+    }
+}
