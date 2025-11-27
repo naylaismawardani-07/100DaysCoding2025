ce939fc7a371f3426e67864aa592fb1bf0a6348b
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-11-27 21:24:52 +0700

    Day81.java

---
+++ Day81.java
@@ -0,0 +1,15 @@
+package Belajar;
+
+public class Day81 {
+    public static void main(String[] args) {
+
+        int[] angka = {10, 20, 30, 40, 50};
+
+        System.out.println("Elemen indeks 0: " + angka[0]);
+        System.out.println("Elemen indeks 1: " + angka[1]);
+        System.out.println("Elemen indeks 2: " + angka[2]);
+        System.out.println("Elemen indeks 3: " + angka[3]);
+        System.out.println("Elemen indeks 4: " + angka[4]);
+
+    }
+}
