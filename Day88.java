c2973159951ede272a758359deec819e844b714b
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-12-04 18:09:47 +0700

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,11 @@
+package Belajar;
+
+public class Day88 {
+    public static void main(String[] args) {
+        int[] angka = {10, 20, 30, 40, 50};
+        for (int i : angka) {
+            System.out.println(i);
+        }
+    }
+    
+}
