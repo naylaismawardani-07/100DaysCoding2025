3ab2af345d34d6c0f0bd6027ffdc7705b3507530
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-12-07 17:45:08 +0700

    Day91.java

---
+++ Day91.java
@@ -0,0 +1,12 @@
+package Belajar;
+
+public class Day91 {
+    static void tambah(int a, int b){
+        System.out.println(a + b);
+    }
+    public static void main(String[] args) {
+        tambah(5, 3);
+        tambah(10, 2);
+    }
+    
+}
