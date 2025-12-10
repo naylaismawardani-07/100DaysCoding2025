5f7e1fa14e7abdcbcf3025cbc3ce805bb51ec9ec
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-12-10 11:17:00 +0700

    Day94.java

---
+++ Day94.java
@@ -0,0 +1,17 @@
+package Belajar;
+
+import java.util.Scanner;
+
+public class Day94 {
+    static void kamu(String nama){
+        Scanner n = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String namaa = n.nextLine();
+        System.out.println("HAII "+namaa);
+        
+    }
+    public static void main(String[] args) {
+        kamu(null);
+    }
+    
+}
