39679d1978cc3a2c86731501667d3e13957879aa
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-12-11 18:29:43 +0700

    Day 95.java

---
+++ Day 95.java
@@ -0,0 +1,12 @@
+package Belajar;
+
+public class Day95 {
+    static void parameter(String para){
+        System.out.println("Halo "+para);
+    }
+    public static void main(String[] args) {
+        parameter("Nayla");
+
+    }
+    
+}
