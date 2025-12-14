407520da0b3c8f4c2b14ecfc89690cc9e0c534f3
naylaismawardani-07 <ismawardaninayla@gmail.com>
2025-12-14 18:53:59 +0700

    Day98.java

---
+++ Day98.java
@@ -0,0 +1,22 @@
+package Belajar;
+
+import java.util.Scanner;
+
+public class Day98 {
+    public static void main(String[] args) {
+        Scanner n = new Scanner(System.in);
+        System.out.print("Masukkan jumlah bilangan Fibonacci: ");
+        int bil = n.nextInt();
+
+        int a = 0;
+        int b = 1;
+
+        System.out.println("Bilangan Fibonacci:");
+        for (int i = 0; i < bil; i++) {
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+}
